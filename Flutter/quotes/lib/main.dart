1860310345beb069f63b8749c1b5c7028d49f115
Kacper Marcinkiewicz <marcinkiewicz.kacper@gmail.com>
2019-11-23 13:31:59 +0100

    Finished project - quotes

--- Flutter/quotes/lib/main.dart
+++ Flutter/quotes/lib/main.dart
@@ -1,4 +1,45 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
 
-void main() => runApp(MaterialApp());
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+  title: "quotes - List of Data",
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(author: "Oscar Wilde", text: "Be your self"),
+    Quote(author: "Oscar Wilde", text: "The truth is reraly pure ..."),
+    Quote(author: "Oscar Wilde", text: "I have nothing to declare"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text("Quotes"),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => QuoteCard(
+          quote: quote,
+          delete: () {  //This will be run from onPressed from quote_card.dart
+            setState(() {
+              quotes.remove(quote);
+            });
+          }
+        )).toList(),
+      ),
+    );
+  }
+}
 
---
+++ Flutter/quotes/lib/quote.dart
@@ -0,0 +1,12 @@
+class Quote {
+
+  String text;
+  String author;
+
+  Quote({ this.text,  this.author }); //Short version
+  
+  // Quote(String text, String author){
+  //   this.text =  text;
+  //   this.author = author;
+  // }
+}
